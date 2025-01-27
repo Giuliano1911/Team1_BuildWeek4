14df2770e1068eb19d2b17e1ab83850b5a7e9166
Giuliano1911 <giulianotorrisi1911@yahoo.com>
2025-01-27 11:51:24 +0100

    setup dependencies and entityManager

---
+++ src/main/java/org/trasporti/EntityManagerUtil.java
@@ -0,0 +1,17 @@
+package org.trasporti;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class EntityManagerUtil {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestionetrasportiPU");
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public static void close() {
+        emf.close();
+    }
+}
